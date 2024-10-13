5df4f51ae13ec9377f56fbfd498a88a947475dfc
aditi-chaturvedi <aditiusha695@gmail.com>
2024-10-13 23:31:33 +0530

    detecting vehicles

---
+++ main.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import cv2
+
+
+from sort.sort import *
+from util import get_car, read_license_plate, write_csv
+
+
+results = {}
+
+mot_tracker = Sort()
+
+# load models
+coco_model = YOLO('yolov8n.pt')
+license_plate_detector = YOLO('./models/license_plate_detector.pt')
+
+# load video
+cap = cv2.VideoCapture('./sample.mp4')
+
+vehicles = [2, 3, 5, 7]
+
+# read frames
+frame_nmr = -1
+ret = True
+while ret:
+    frame_nmr += 1
+    ret, frame = cap.read()
+    if ret:
+        results[frame_nmr] = {}
+        # detect vehicles
+        detections = coco_model(frame)[0]
+        detections_ = []
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = detection
+            if int(class_id) in vehicles:
+                detections_.append([x1, y1, x2, y2, score])
